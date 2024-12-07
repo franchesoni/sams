8a7570a2e43718c133e11b50963d502330bf1f8d
franchesoni <marchesoniacland@gmail.com>
2024-12-07 17:03:38 +0100

    automatically download

---
+++ run_download_hypersim_segmentation.sh
@@ -0,0 +1,3 @@
+python download_hypersim.py --contains images/scene_cam_00_final_preview/frame.0000.color.jpg
+python download_hypersim.py --contains images/scene_cam_00_geometry_hdf5/frame.0000.render_entity_id.hdf5
+
