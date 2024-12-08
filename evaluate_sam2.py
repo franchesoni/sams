89d8ea3d7f115a3fc40fa1caa189b498cfbc7692
franchesoni <marchesoniacland@gmail.com>
2024-12-08 16:13:53 +0100

    factorize, use bf16 only for this script

--- evaluate_sam2.py
+++ evaluate_sam2.py
@@ -10,14 +10,15 @@ from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator, rle_to_mas
 
 from dataset import HypersimSegmentationDataset
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if device.type == "cuda":
-    # use bfloat16 for the entire notebook
-    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
-    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
-    if torch.cuda.get_device_properties(0).major >= 8:
-        torch.backends.cuda.matmul.allow_tf32 = True
-        torch.backends.cudnn.allow_tf32 = True
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if device.type == "cuda":
+        # use bfloat16 for the entire notebook
+        torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+        # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+        if torch.cuda.get_device_properties(0).major >= 8:
+            torch.backends.cuda.matmul.allow_tf32 = True
+            torch.backends.cudnn.allow_tf32 = True
 
 
 def minmax(x):
@@ -56,6 +57,36 @@ def get_mask_generator(
     return mask_generator
 
 
+def sample_result(masks, labels, device):
+    """
+    masks: (M, H, W), np.ndarray
+    labels: (H, W), np.ndarray
+    """
+    H, W = labels.shape
+    masks = torch.from_numpy(masks).reshape(-1, H * W).to(device)  # (M, HW)
+    labels = torch.from_numpy(labels).reshape(1, H * W).to(device)
+    max_ious = []
+    unique_labels = torch.unique(labels)
+    for label in unique_labels:
+        label_mask = labels == label  # (1, HW)
+        if label_mask.any():  # If not empty
+            intersection = (masks & label_mask).sum(dim=1)  # (M,)
+            union = (masks | label_mask).sum(dim=1)  # (M,)
+            ious = intersection / union  # (M,)
+            max_iou = ious.max()
+            max_ious.append(max_iou.item())
+            # print(
+            #     f"Max IoU for label {label.item()}: {max_iou.item()}, average: {ious.mean().item()}",
+            #     end="\r",
+            # )
+    return {
+        "L": len(unique_labels),
+        "M": len(masks),
+        "max_ious": max_ious,
+        "mIoU": ious.mean().item(),
+    }
+
+
 def main(
     points_per_side=32,
     stability_score_thresh=0.75,
@@ -83,29 +114,7 @@ def main(
         masks, logits, predicted_ious, stability_scores = generate_masks(
             None, image, mask_generator
         )
-        masks = torch.from_numpy(masks).reshape(-1, H * W).to(device)  # (M, HW)
-        labels = torch.from_numpy(labels).reshape(1, H * W).to(device)
-        max_ious = []
-        for label in torch.unique(labels):
-            label_mask = labels == label  # (1, HW)
-            if label_mask.any():  # If not empty
-                intersection = (masks & label_mask).sum(dim=1)  # (M,)
-                union = (masks | label_mask).sum(dim=1)  # (M,)
-                ious = intersection / union  # (M,)
-                max_iou = ious.max()
-                max_ious.append(max_iou.item())
-                # print(
-                #     f"Max IoU for label {label.item()}: {max_iou.item()}, average: {ious.mean().item()}",
-                #     end="\r",
-                # )
-        res_per_sample.append(
-            {
-                "L": len(torch.unique(labels)),
-                "M": len(masks),
-                "max_ious": max_ious,
-                "mIoU": ious.mean().item(),
-            }
-        )
+        res_per_sample.append(sample_result(masks, labels, device))
 
     np.save(
         "res_per_sample.npy",
